74f3c9550521320b72285a1e964262f633482f34
Beckers, G.J.L. (Gabriël) <g.j.l.beckers@uu.nl>
2019-10-02 17:14:06 +0200

    improve test unknown array type

--- darr/tests/test_array.py
+++ darr/tests/test_array.py
@@ -401,6 +401,8 @@ class TestReadArrayDescr(DarrTestCase):
         with tempdir() as dirname:
             dar = create_array(path=dirname, shape=(2,4), fill=0,
                                dtype='int64', overwrite=True)
+            dar._arrayinfo['arrayorder'] = 'X'
+            self.assertRaises(ValueError, numtypedescriptiontxt, dar)
             dar._update_jsondict(dar._arraydescrpath.absolute(),
                                  {'arrayorder': 'X'})
             self.assertRaises(ValueError, Array, dirname)
