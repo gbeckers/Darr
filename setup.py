dccabf9026381c9d2edfb29040dd73a024ba6ada
Gabriel <gabriel@gbeckers.nl>
2020-05-12 18:29:28 +0200

    add Python 3.8 classifier

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         'Development Status :: 3 - Alpha',
