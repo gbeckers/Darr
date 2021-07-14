066cb79b33eb22041921ef08cc7d162055246098
Gabriel <gabriel@gbeckers.nl>
2021-07-14 11:44:07 +0200

    update versioneer

--- darr/__init__.py
+++ darr/__init__.py
@@ -3,6 +3,5 @@ from .raggedarray import *
 from .datadir import DataDir, create_datadir
 from .tests import test
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
