fdd58b0f0c6b2887cf7e9cf43a46ba84b822f546
Gabriel Beckers <gabriel@gbeckers.nl>
2022-03-05 09:27:08 +0100

    create idl testcode

--- create_examplearrays.py
+++ create_examplearrays.py
@@ -21,9 +21,9 @@ def create_arrays(basepath='.'):
         "datetime": f"{datetimestring()}",
         "samplingrate": 25000.0
     }
-    ar = [[1, 2], [3, 4], [4, 6], [7, 8],
-          [9, 10], [11, 12], [13, 14], [15, 16]]
-    car = np.array(ar, dtype='complex128') + 1.3j
+    # 2D
+    ar = np.arange(0,16).reshape(8,2) # sums to 120, axis 0 sums to 56, 1 to 64
+    car = np.array(ar, dtype='complex128') + 2.0j
     for numtype in numtypesdescr.keys():
         if numtype.startswith('complex'):
             a = car
@@ -31,8 +31,9 @@ def create_arrays(basepath='.'):
             a = ar
         darr.asarray(basepath / f'array_{numtype}_2D.darr', a,
                      dtype=numtype, metadata=metadata, overwrite=True)
-    ar = [1, 3, 5, 7, 9, 11, 13]
-    car = np.array(ar, dtype='complex128') + 1.3j
+    # 1D
+    ar = [1, 3, 5, 7, 9, 11, 14] # sums to 50
+    car = np.array(ar, dtype='complex128') + 2.0j
     for numtype in numtypesdescr.keys():
         if numtype.startswith('complex'):
             a = car
@@ -72,6 +73,22 @@ def create_raggedarrays(basepath='.'):
         _ = darr.asraggedarray(basepath / f'raggedarray_{numtype}.darr',
                                ar, dtype=numtype, metadata=metadata,
                                overwrite=True)
+
+
+def create_codefile_array_idl(arraydirpath):
+    allcode = []
+    for arraypath in Path(arraydirpath).glob('*.darr'):
+        ar = darr.Array(arraypath)
+        code = ar.readcode('idl', abspath=True)
+        if code is not None:
+            allcode.append(f'; {arraypath.name}')
+            allcode.append(code)
+            allcode.append(f'TOTAL(a)')
+            if len(ar.shape)>1:
+                allcode.append(f'TOTAL(a[0,*])\n')
+                allcode.append(f'TOTAL(a[1,*])\n')
+    return '\n'.join(allcode)
+
 
 
 if __name__ == "__main__":
